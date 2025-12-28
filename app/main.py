82c7bf519407d209e32fddab1771c2da937cfa19
datshroud <datshroud@LAPTOP-JK88VE84.localdomain>
2025-12-29 03:02:30 +0700

    refactor to asyncio and add expiry into SET cmd

--- app/main.py
+++ app/main.py
@@ -1,88 +1,124 @@
+import asyncio
 import socket
-import threading  # noqa: F401
+import threading
+import time  # noqa: F401
 
+HOST = "localhost"
+PORT = 6379
 mem = {}
 
-def parseResp(data: bytes):
+async def read_line(reader):
+    # read until meet \r\n
+    buffer = bytearray()
+    while buffer[-2:] != b"\r\n":
+        byte = await reader.readexactly(1)
+        if not byte:
+            return None
+        buffer += byte
+    return buffer[:-2]
+
+
+async def parse_resp(reader):
     # Parse RESP Array, ví dụ:
     # *2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n
     # => ["GET", "foo"]
-    arr = data.decode().split("\r\n")
-    if arr[0][0] != '*':
-        return []
+    first = await read_line(reader)
+    if first is None:
+        return None
+    if first[0:1] != b"*":
+        raise ValueError("Only RESP Array Supported")
+    n = int(first[1:])
     ans = []
-    idx = 1
-    while idx < len(arr):
-        word = arr[idx]
-        # print(word)
-        if len(word) > 0 and word[0] == '$':
-            ans.append(arr[idx + 1])
-            idx += 2
-        else:
-            idx += 1
-        # print(idx, ans)
+    for i in range(n):
+        len_line = await read_line(reader)
+        comment = await read_line(reader)
+        ans.append(comment.decode())
     return ans
 
 
-def handleClient(connection, clientAddress):
-    print("Client connected, address:", clientAddress)
+def is_expired(expire_time):
+    if expire_time is None:
+        return False
+    return time.time() >= expire_time
+
+async def handle_client(reader, writer):
+    addr = writer.get_extra_info("peername")
+    print(f"Client connected: {addr}")
 
     try:
         while True:
-            data = connection.recv(4096)
-            if not data:
-                print("Client disconnected, address:", clientAddress)
+            command = await parse_resp(reader)
+            if not command:
                 break
-            print(f"Raw data from {clientAddress}: {data}")
-            command = parseResp(data)
             cmd = command[0].upper()
             # print(f"command: {command}, cmd = {cmd}, len cmd = {len(command)}")
             if cmd == "PING":
-                connection.sendall(b"+Pong\r\n")
+                writer.write(b"+Pong\r\n")
             elif cmd == "ECHO" and len(command) == 2:
                 msg = command[1]
                 resp = f"${len(msg)}\r\n{msg}\r\n".encode()
-                connection.sendall(resp)
-            elif cmd == "SET" and len(command) == 3:
+                # connection.sendall(resp)
+                writer.write(resp)
+            elif cmd == "SET":
                 key = command[1]
                 value = command[2]
-                mem[key] = value
-                connection.sendall(b"+OK\r\n")
+                expire_at = None
+                if len(command) >= 5:
+                    opt = command[3].upper()
+                    t = float(command[4])
+                    sec = t if opt == "PX" else t / 1000
+                    expire_at = time.time() + sec
+                mem[key] = (value, expire_at)
+
+
+                # connection.sendall(b"+OK\r\n")
+                writer.write(b"+OK\r\n")
             elif cmd == "GET" and len(command) == 2:
                 key = command[1]
                 if key in mem:
-                    value = mem[key]
-                    resp = f"${len(value)}\r\n{value}\r\n".encode()
-                    connection.sendall(resp)
+                    value, expire_at = mem[key]
+                    if is_expired(expire_at):
+                        del mem[key]
+                        writer.write(b"$-1\r\n")
+                    else:
+                        resp = f"${len(value)}\r\n{value}\r\n".encode()
+                        # connection.sendall(resp)
+                        writer.write(resp)
                 else:
-                    connection.sendall(b"$-1\r\n") # null bulk string
+                    # connection.sendall(b"$-1\r\n") # null bulk string
+                    writer.write(b"$-1\r\n")
             else:
-                connection.sendall(b"-ERR unknown command\r\n")
-    except Exception as e:
-        print(f"Error with {clientAddress}: {e}")
+                writer.write(b"-ERR unknown command\r\n")
+    except asyncio.IncompleteReadError:
+        pass
     finally:
-        connection.close()
+        writer.close()
+        await writer.wait_closed()
+        print(f"Client disconnected: {addr}")
             
 
-def main():
+async def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     # Uncomment the code below to pass the first stage
     #
-    serverSocket = socket.create_server(("localhost", 6379))
-    serverSocket.listen()
+    # serverSocket = socket.create_server(("localhost", 6379))
+    # serverSocket.listen()
+    server = await asyncio.start_server(handle_client, HOST, PORT)
     print(f"Server listening on port 6379.")
     # connection.sendall(b"+PONG\r\n")
 
-    while True:
-        connection, clientAddress = serverSocket.accept()
-        clientThread = threading.Thread(
-            target=handleClient,
-            args=(connection, clientAddress),
-            daemon=True
-        )
-        clientThread.start()
+    # while True:
+    #     connection, clientAddress = serverSocket.accept()
+    #     clientThread = threading.Thread(
+    #         target=handle_client,
+    #         args=(connection, clientAddress),
+    #         daemon=True
+    #     )
+    #     clientThread.start()
+    async with server:
+        await server.serve_forever()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
